83a06571a4b41cd7e79e61a48c52cc3da773a65c
yeebinrong <yeebinrong@gmail.com>
2020-12-07 15:22:46 +0800

    'initial'

---
+++ main.js
@@ -0,0 +1,93 @@
+// load libraries
+const express = require('express')
+const morgan = require('morgan')
+const mysql = require('mysql2/promise')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+let secureEnv = require('secure-env');
+
+// create an instance of express
+const app = express()
+
+// declare variables
+global.env = secureEnv({secret: process.env.ENV_PASSWORD});
+const PORT = parseInt(process.argv[2]) || parseInt(process.env.PORT) || 3000
+
+// create pool
+const pool = mysql.createPool({
+    host: global.env.SQL_HOST,
+    port: global.env.SQL_PORT,
+    user: global.env.SQL_USER,
+    password: global.env.SQL_PASS,
+    database: global.env.SQL_SCHEMA,
+    connectionLimit: global.env.SQL_CON_LIMIT
+})
+
+// cors for browser cors bypass
+app.use(cors())
+
+// parse data
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.use(bodyParser.json({limit: '50mb'}))
+
+// morgan for logging
+app.use(morgan("combined"))
+
+// SQL QUERIES
+const SELECT_ALL = 'SELECT * FROM rsvp'
+const INSERT_NEW = `INSERT INTO rsvp (name, email, phone, status, createdBy, createdDt) values (?,?,?,?,?,CURDATE())`
+
+const mkQuery = (sqlStmt, pool) => {
+    return async (params) => {
+        // get a connection from pool
+        const conn = await pool.getConnection()
+        try {
+            // Execute the query with the parameter
+            const results = await conn.query(sqlStmt, params)
+            return results[0]
+        } catch (e) {
+            return Promise.reject(e)            
+        } finally {
+            conn.release()
+        }
+    }
+}
+
+const SELECT_ALL_QUERY = mkQuery(SELECT_ALL, pool)
+const INSERT_NEW_QUERY = mkQuery(INSERT_NEW, pool)
+
+SELECT_ALL_QUERY()
+.then(data => {
+    console.info(data);
+}) 
+
+// app.use(express.static(`${__dirname}/client/src`))
+
+app.get('/api', (req, resp) => {
+    resp.status(200)
+    resp.type('text/html')
+    resp.sendFile(`${__dirname}/index.html`)
+})
+
+// initalise the app
+const startApp = async (app, pool) => {
+    try {
+        // get connection from db
+        const conn = await pool.getConnection()
+        console.info('Pinging database...')
+        await conn.ping()
+
+        //release connection
+        conn.release()
+        console.info('Successfully pinged database...')
+        // listen for port
+        app.listen(PORT, () => {
+            console.info(`Application is listening to PORT ${PORT} at ${new Date()}.`)
+        })
+    } 
+    catch (e) {
+        console.error("Error pinging database! ", e)
+    }
+}
+
+startApp(app, pool)
